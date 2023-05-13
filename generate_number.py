41cebbc640e794a3bf4d4f9e0517a8acb454fd16
Ye Zeiya Shein <yezeiyashein@gmail.com>
2023-05-13 22:58:17 +0630

    Update generate_number.py

--- generate_number.py
+++ generate_number.py
@@ -1,18 +1,28 @@
 import random
+import os
 
-start = 12
-end = 62
+file_path = 'excluded.txt'
 
-excluded_numbers = []
-with open('excluded.txt', 'r') as f:
-    for line in f:
-        for excluded_number in line.split(','):
-            excluded_numbers.append(int(excluded_number.strip()))
+if not os.path.isfile(file_path):
+    open(file_path, 'a').close()
+
+with open(file_path, 'r') as f:
+    excluded_numbers = [int(x) for x in f.read().split(',') if x.strip()]
+
+start = 1
+end = 100
+count = 3
 
 random_numbers = []
-while len(random_numbers) < 3:
+
+while len(random_numbers) < count:
     random_number = random.randint(start, end)
-    if random_number not in excluded_numbers:
+    if random_number not in excluded_numbers and random_number not in random_numbers:
         random_numbers.append(random_number)
 
-print("Random Question Numbers:", ", ".join(str(number) for number in random_numbers))
+with open(file_path, 'a') as f:
+    if excluded_numbers:
+        f.write(',')
+    f.write(','.join(str(x) for x in random_numbers))
+
+print('Random Question Numbers:', ', '.join(str(x) for x in random_numbers))
